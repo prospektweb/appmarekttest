5bf2999b95260a9576adf24a740183bc9e80edb8
PROSPEKT <prospektweb@gmail.com>
2025-12-06 08:25:30 +0500

    Add initial settings configuration for controllers

---
+++ .settings.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'controllers' => [
+        'value' => [
+            'namespaces' => [
+                '\\Prospektweb\\Calc\\Controllers' => 'controllers',
+            ],
+        ],
+        'readonly' => true,
+    ],
+];
