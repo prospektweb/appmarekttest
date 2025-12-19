856718efbf78030bc98ed2ad2cba82745afa929c
PROSPEKT <prospektweb@gmail.com>
2025-12-19 07:32:55 +0500

    Create default_option.php

---
+++ default_option.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Настройки модуля prospektweb.calc по умолчанию
+ */
+
+$prospektweb_calc_default_option = [
+    'DEFAULT_PRICE_TYPE_ID' => 1,
+    'DEFAULT_CURRENCY' => 'RUB',
+    'LOGGING_ENABLED' => 'N',
+    // Инфоблоки для интеграции с React-калькулятором
+    'IBLOCK_MATERIALS' => 0,
+    'IBLOCK_OPERATIONS' => 0,
+    'IBLOCK_EQUIPMENT' => 0,
+    'IBLOCK_DETAILS' => 0,
+    'IBLOCK_CALCULATORS' => 0,
+    'IBLOCK_CONFIGURATIONS' => 0,
+    'PROPERTY_CONFIG_ID' => 'CONFIG_ID',
+];
