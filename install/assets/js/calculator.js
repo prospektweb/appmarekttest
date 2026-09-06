7814faf828f6982e441da6956a0a851b4513b1b5
PROSPEKT <prospektweb@gmail.com>
2026-09-06 18:06:47 +0500

    build(calcconfig): scope split workspace editor portals

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=0de02e90cc5f',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=400242a7eb3c',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
--- install/version.php
+++ install/version.php
@@ -1,5 +1,5 @@
 <?php
 $arModuleVersion = [
-    'VERSION' => '1.13.110',
-    'VERSION_DATE' => '2026-09-06 17:40:13',
+    'VERSION' => '1.13.111',
+    'VERSION_DATE' => '2026-09-06 18:04:06',
 ];
