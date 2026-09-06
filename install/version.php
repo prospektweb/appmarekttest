7c41c814fcb5c962b13ae3c01e08c7256b8b74f1
PROSPEKT <prospektweb@gmail.com>
2026-09-06 16:27:33 +0500

    Release exact catalog search and compact tree result cards

--- install/version.php
+++ install/version.php
@@ -1,5 +1,5 @@
 <?php
 $arModuleVersion = [
-    'VERSION' => '1.13.105',
-    'VERSION_DATE' => '2026-09-06 16:11:07',
+    'VERSION' => '1.13.106',
+    'VERSION_DATE' => '2026-09-06 16:27:32',
 ];
