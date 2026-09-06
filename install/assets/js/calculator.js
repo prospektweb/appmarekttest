c015b5444a22dcd341718cb99f9b9cf9d5ec38ba
PROSPEKT <prospektweb@gmail.com>
2026-09-06 17:10:30 +0500

    Release 1920px calculation logic slider limit

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=2b2ffcfaa981',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=0de02e90cc5f',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
--- install/version.php
+++ install/version.php
@@ -1,5 +1,5 @@
 <?php
 $arModuleVersion = [
-    'VERSION' => '1.13.108',
-    'VERSION_DATE' => '2026-09-06 17:06:19',
+    'VERSION' => '1.13.109',
+    'VERSION_DATE' => '2026-09-06 17:10:21',
 ];
