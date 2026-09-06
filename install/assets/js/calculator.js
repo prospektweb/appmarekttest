be2b2825389ff5d38aa8d60733735e6f0f6a01f7
PROSPEKT <prospektweb@gmail.com>
2026-09-06 15:29:13 +0500

    release: fix encoded material parameter wiring in calculator 1.13.101

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=664de955d276',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=208537eefe55',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
--- install/version.php
+++ install/version.php
@@ -1,5 +1,5 @@
 <?php
 $arModuleVersion = [
-    'VERSION' => '1.13.100',
-    'VERSION_DATE' => '2026-09-06 15:30:00',
+    'VERSION' => '1.13.101',
+    'VERSION_DATE' => '2026-09-06 15:27:55',
 ];
